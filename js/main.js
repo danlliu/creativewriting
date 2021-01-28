ae1620de834de29a51d9f9e32b7e2350d7439e20
danlliu <danlliu@umich.edu>
2021-01-27 20:25:51 -0500

    looks good, I added in the run button and starting on the javascript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+let output_samp = document.querySelector('#output');
+let text_field = document.querySelector('textarea');
+
+function run() {
+    let text = text_field.value
+    let paragraphs = text.split("\n");
+
+}
