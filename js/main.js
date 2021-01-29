b8deca9f18696feab87bd9bea37205f0aa3dd6ce
danlliu <danlliu@umich.edu>
2021-01-28 20:21:52 -0500

    implemented everything, working on debug mode because annoying skip logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,256 @@
 
-let output_samp = document.querySelector('#output');
-let text_field = document.querySelector('textarea');
+let output_samp = document.getElementById('output');
+let text_field = document.getElementById('program');
+let debug_text = document.getElementById('debug');
+
+const KEYWORDS = [
+    "to", "the", "after",
+    "am", "i'm", "be", "are", "you're", "we're", "they're", "is", "he's", "she's", "it's", "was", "were", "being", "been",
+    "took", "take", "gave", "give", "these", "no", "out", "say", "any", "not", "all", "after", "use", "using", "used",
+    "none", "must", "like", "go", "back", "if", "other", "over"
+];
+
+const REGISTERS = [
+    "am", "i'm", "be", "are", "you're", "we're", "they're", "is", "he's", "she's", "it's", "was", "were", "being", "been"
+];
+
+let registers = [0, 0, 0, 0, 0, 0, 0, 0];
+let stack = [];
+let output = "";
+let output_err = false;
+
+function hide_debug() {
+    text_field.removeAttribute('hidden');
+    debug_text.setAttribute('hidden', "spawn more overlords");
+    document.getElementById('sh_dbg').removeAttribute('hidden');
+    document.getElementById('hd_dbg').setAttribute('hidden', 'hehe');
+}
+
+function show_debug() {
+    debug_text.removeAttribute('hidden');
+    text_field.setAttribute('hidden', "you must construct additional pylons");
+    document.getElementById('hd_dbg').removeAttribute('hidden');
+    document.getElementById('sh_dbg').setAttribute('hidden', 'hehe');
+
+    // sTyLiStIc tExT
+}
 
 function run() {
-    let text = text_field.value
-    let paragraphs = text.split("\n");
+
+    registers = [0, 0, 0, 0, 0, 0, 0, 0];
+    stack = [];
+    output = "";
+    output_err = false;
+
+    let text = text_field.value.toLowerCase();
+    text.replace(/[^A-Za-z']/gi);
+    let paragraphs = text.split(/[\n]+/);
+    let words = paragraphs.map(s => s.split(" "));
+    let current_paragraph = 0;
+    let word_idx = 0;
+
+    console.log(words);
+
+    let state = "none";
+    let branch_target = 0;
+
+    let steps = 0;
+
+    while (current_paragraph !== paragraphs.length && !output_err && steps <= 1000) {
+        console.log(`currpar ${current_paragraph}, currwor ${word_idx}`);
+        let word = words[current_paragraph][word_idx];
+        console.log(state);
+        console.log(word);
+        if (state === "skip") {
+            if (KEYWORDS.includes(word)) {
+                state = "none";
+            }
+        }
+        else if (state === "after") {
+            if (word !== "the") {
+                stack.push(word.length);
+                state = "none";
+            }
+            else {
+                state = "after the";
+            }
+        }
+        else if (state === "after the") {
+            stack.push(word.length);
+            state = "none";
+        }
+        else if (state === "take") {
+            if (REGISTERS.includes(word)) {
+                // register!
+                if (stack.length === 0) {
+                    output_err = true;
+                    output += `<br>Error! Trying to take from the stack when the stack is empty!`;
+                } else {
+                    registers[REGISTERS.indexOf(word)] = stack.pop();
+                    state = "none";
+                }
+            }
+        }
+        else if (state === "give") {
+            if (REGISTERS.includes(word)) {
+                // register!
+                stack.push(registers[REGISTERS.indexOf(word)]);
+                state = "none";
+            }
+        }
+        else if (state === "if") {
+            if (word === "go") {
+                if (words[current_paragraph][word_idx + 1] === "back") {
+                    registers[7] = current_paragraph;
+                }
+                let a = stack.pop();
+                let b = stack.pop();
+                if (a === b) {
+                    current_paragraph = branch_target - 1;
+                    word_idx = 0;
+                    state = "none";
+                    continue;
+                } else {
+                    state = "none";
+                }
+            }
+        }
+        else {
+            switch (word) {
+                case "to":
+                case "the":
+                    state = "skip";
+                    break;
+                case "these":
+                    // add
+                    if (stack.length < 2) {
+                        output_err = true;
+                        output += `<br>Error! Trying to add top two elements on stack, but stack doesn't have 2 elements!`;
+                    } else {
+                        let a = stack.pop();
+                        let b = stack.pop();
+                        stack.push(a + b);
+                    }
+                    break;
+                case "no":
+                    if (stack.length === 0) {
+                        output_err = true;
+                        output += `<br>Error! Cannot negate top of empty stack since there is no top!`;
+                    } else {
+                        let a = stack.pop();
+                        stack.push(-a);
+                    }
+                    break;
+                case "all":
+                    if (stack.length < 2) {
+                        output_err = true;
+                        output += `<br>Error! Trying to bitwise AND top two elements on stack, but stack doesn't have 2 elements!`;
+                    } else {
+                        let a = stack.pop();
+                        let b = stack.pop();
+                        stack.push(a & b);
+                    }
+                    break;
+                case "any":
+                    if (stack.length < 2) {
+                        output_err = true;
+                        output += `<br>Error! Trying to bitwise OR top two elements on stack, but stack doesn't have 2 elements!`;
+                    } else {
+                        let a = stack.pop();
+                        let b = stack.pop();
+                        stack.push(a | b);
+                    }
+                    break;
+                case "not":
+                    if (stack.length === 0) {
+                        output_err = true;
+                        output += `<br>Error! Trying to invert bits of top of empty stack!`;
+                    } else {
+                        let a = stack.pop();
+                        stack.push(~a);
+                    }
+                    break;
+                case "must":
+                    if (stack.length < 2) {
+                        output_err = true;
+                        output += `<br>Error! Trying to find max of top two elements on stack, but stack doesn't have 2 elements!`;
+                    } else {
+                        let a = stack.pop();
+                        let b = stack.pop();
+                        stack.push(a > b ? a : b);
+                    }
+                    break;
+                case "take":
+                case "took":
+                    state = "take";
+                    break;
+                case "gave":
+                case "give":
+                    state = "give";
+                    break;
+                case "after":
+                    state = "after";
+                    break;
+                case "none":
+                    stack.push(0);
+                    break;
+                case "use":
+                case "using":
+                case "used":
+                    stack.pop();
+                    break;
+                case "like":
+                    if (stack.length === 0) {
+                        output_err = true;
+                        output += `<br>Error! Trying to copy the top of an empty stack!`;
+                    }
+                    let e = stack.pop();
+                    stack.push(e);
+                    stack.push(e);
+                    break;
+                case "if":
+                    state = "if";
+                    branch_target = stack.pop();
+                    break;
+                case "go":
+                    branch_target = stack.pop();
+                    current_paragraph = branch_target - 1;
+                    word_idx = -1; // gets incremented to 0
+                    break;
+                case "other":
+                case "over":
+                    current_paragraph += 1;
+                    word_idx = 0;
+                    break;
+                case "out": {
+                    let top = stack.pop();
+                    stack.push(top);
+                    output += `${top}`;
+                }
+                    break;
+                case "say": {
+                    let top = stack.pop();
+                    stack.push(top);
+                    output += `${String.fromCharCode(top)}`;
+                }
+                    break;
+            }
+        }
+        console.log(stack);
+        console.log(current_paragraph);
+        word_idx += 1;
+        steps += 1;
+        if (word_idx === words[current_paragraph].length) {
+            word_idx = 0;
+            current_paragraph += 1;
+        }
+    }
+
+    output_samp.innerHTML = output;
+    if (output_err) {
+        output_samp.style.color = "#F00";
+    } else {
+        output_samp.style.color = "#FFF";
+    }
 
 }
